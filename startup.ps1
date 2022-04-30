c4a03a4599722600f026ef7ac4143d70d1022487
James Kong <jameskong098@gmail.com>
2022-04-30 16:28:56 -0400

    got rid of scripts on github

--- startup.ps1
+++
@@ -1,2 +0,0 @@
-$Env.mongodb_URI = 'mongodb+srv://jameskong:<password>@cs103a-cpa02.dq9w5.mongodb.net/KongData?retryWrites=true&w=majority'
-npm start
--- startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export mongodb_URI='mongodb+srv://jameskong:<password>@cs103a-cpa02.dq9w5.mongodb.net/KongData?retryWrites=true&w=majority'
-echo "connecting to $mongodb_URI"
-nodemon
